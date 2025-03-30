efec4c0449ebaf79cf961c9b7af24759714ba98f
Isurunath99 <isurunathnanayakkara@gmail.com>
2025-03-30 11:30:26 +0530

    Initial Commit

---
+++ PetPal/PetPalApp.swift
@@ -0,0 +1,17 @@
+//
+//  PetPalApp.swift
+//  PetPal
+//
+//  Created by sasiri rukshan nanayakkara on 3/30/25.
+//
+
+import SwiftUI
+
+@main
+struct PetPalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PetPalTests/PetPalTests.swift
@@ -0,0 +1,17 @@
+//
+//  PetPalTests.swift
+//  PetPalTests
+//
+//  Created by sasiri rukshan nanayakkara on 3/30/25.
+//
+
+import Testing
+@testable import PetPal
+
+struct PetPalTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
